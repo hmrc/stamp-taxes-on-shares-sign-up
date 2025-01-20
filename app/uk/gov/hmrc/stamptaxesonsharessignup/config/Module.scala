2d17d4b112c6986bdd8d7ccd8940b27a270d8a91
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2025-01-20 10:41:58 +0000

    'Creating new service stamp-taxes-on-shares-sign-up'

---
+++ app/uk/gov/hmrc/stamptaxesonsharessignup/config/Module.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.stamptaxesonsharessignup.config
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+
+  override def configure(): Unit = {
+
+    bind(classOf[AppConfig]).asEagerSingleton()
+  }
+}
