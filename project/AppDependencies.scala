465c42450f61cd3096984920a0c44699de260f71
jon-humble-hmrc <194776673+jon-humble-hmrc@users.noreply.github.com>
2025-03-25 13:54:35 +0000

    Latest versions of deps plus new test deps and latest sbt

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,20 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "9.9.0"
-  
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
+    "org.scalactic"           %% "scalactic"                  % "3.2.19",
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"         % bootstrapVersion  % Test,
+    "org.scalatest"           %% "scalatest"                      % "3.2.19"          % Test,
+    "org.scalatestplus.play"  %% "scalatestplus-play"             % "7.0.1"           % Test,
+    "org.scalacheck"          %% "scalacheck"                     % "1.18.1"          % Test,
+    "org.scalatestplus"       %% "scalacheck-1-18"                % "3.2.19.0"        % Test,
+    "org.mockito"             %  "mockito-core"                   % "5.16.1"          % Test,
+    "org.scalatestplus"       %% "mockito-5-12"                   % "3.2.19.0"        % Test,
   )
 
   val it = Seq.empty
